e38e2c93c1db87469a0f455573351958d80ad889
IronShell72 <unknown.objc@gmail.com>
2021-04-23 16:39:22 +0300

    Links

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
 import threading
 import telegram
+import requests
 
+#99
 def main():
+    url = "https://rest.coinapi.io/v1/exchangerate/BTC?apikey=35A30795-914A-447C-9238-9265B9DB55C4"
     pass
 def check():
     pass
+
+#https://rest.coinapi.io/v1/exchangerate/LTC/USDT?apikey=35A30795-914A-447C-9238-9265B9DB55C4
+#https://docs.coinapi.io/#endpoints-2
